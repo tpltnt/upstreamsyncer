068dd517ce7a499ef81685f0c78c0c1a9937f19c
tpltnt <tpltnt.github@dropcut.net>
2018-04-16 20:54:08 +0200

    added basic script

---
+++ upstreamsyncer.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# a shell script to sync the "master" branch
+# of a git repository with the "master" branch
+# with the upstream repository.
+set -e
+
+# determine if upstream exists
+SHOULD_SYNC=false
+for remote in `git remote`; do
+    if [ "$remote" = "upstream" ]; then
+	SHOULD_SYNC=true
+    fi
+done
+
+if [ "$SHOULD_SYNC" = false ]; then
+    echo "no upstream to sync"
+    exit 0  # nothing to do
+fi
+
+# store current branch
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+# sync with upstream
+git checkout master
+git pull upstream master
+git push origin master
+
+# go back to current branch
+git checkout $CURRENT_BRANCH
